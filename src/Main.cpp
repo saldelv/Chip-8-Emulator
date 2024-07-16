88e2ba089a968c5c259db8cc92ef8b046cc00b13
Salvatore Del Vecchio <96501610+saldelv@users.noreply.github.com>
2024-07-15 22:15:22 -0400

    SDL setup and test

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,31 @@
 #include "Main.h"
-#include "iostream"
+#include <iostream>
+#include <stdio.h>
+#include <SDL2/SDL.h>
 
-int main() {
-    std::cout << "-------------------------------------" << std::endl;
-    return 0;
+int main(int argv, char** args) {
+    std::cout << "Hello" << std::endl;
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window *window = SDL_CreateWindow("Hello", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 800, SDL_WINDOW_ALLOW_HIGHDPI);
+
+    if (NULL == window) {
+        std::cout << "Could not create window: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Event windowEvent;
+
+    while(true) {
+        if (SDL_PollEvent(&windowEvent)) {
+            if (SDL_QUIT == windowEvent.type) {
+                break;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
 }
