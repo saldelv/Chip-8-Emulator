a99859b7d9f805fadcf563d46b51c55dfcc1c426
Salvatore Del Vecchio <96501610+saldelv@users.noreply.github.com>
2024-07-25 15:14:38 -0400

    SDL audio initialization

--- src/Sound.cpp
+++ src/Sound.cpp
@@ -1,11 +1,31 @@
 #include "Sound.h"
 
+void SDLCALL audioCallback(void *userdata, Uint8 *stream, int len)
+{
+    
+}
+
+
 Sound::Sound()
 {
-   
+    SDL_zero(want);
+    SDL_zero(have);
+
+    want.freq = 48000;
+    want.format = AUDIO_F32;
+    want.channels = 2;
+    want.samples = 4096;
+    want.callback = audioCallback;
+
+    dev = SDL_OpenAudioDevice(NULL, 0, &want, &have, SDL_AUDIO_ALLOW_FORMAT_CHANGE);
 }
 
 void Sound::playSound()
 {
 
+}
+
+void Sound::stopSound()
+{
+    
 }
--- src/Sound.h
+++ src/Sound.h
@@ -2,11 +2,17 @@
 
 #include <stdio.h>
 #include <iostream>
-#include <SDL2/SDL.h>
+#include <SDL2/SDL_audio.h>
 
 class Sound
 {
 public:
     Sound();
     void playSound();
+    void stopSound();
+
+    SDL_AudioSpec want, have;
+    SDL_AudioDeviceID dev;
 };
+
+    void SDLCALL audioCallback(void *userdata, Uint8 *stream, int len);
